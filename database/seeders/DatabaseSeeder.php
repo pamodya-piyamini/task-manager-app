1d1c6f72c2b85717d92b283afada9b46fa02bbfa
Pamodya Piyamini <ppiyamini@gmail.com>
2025-06-19 11:24:36 +0530

    populating test database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Task;
+use App\Models\Category;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        if (User::count() === 0) {
+            $user = User::factory()->create([
+                'name' => 'Test User',
+                'email' => 'test@example.com',
+                'password' => bcrypt('password'),
+            ]);
+        } else {
+            $user = User::first();
+        }
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        if (Category::count() === 0) {
+            Category::factory()->count(3)->create([
+                'user_id' => $user->id,
+            ]);
+        }
+
+        if (Task::count() === 0) {
+            Task::factory()->count(5)->create([
+                'user_id' => $user->id,
+                'category_id' => Category::first()->id,
+            ]);
+        }
     }
 }
